ef3285ef4a8297ee378409f265983d164d02313d
PaulKovalov <soapmactavish799@gmail.com>
2020-08-12 13:10:27 +0300

    board container component

--- sticknotes-frontend/src/app/app-routing.module.ts
+++ sticknotes-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardContainerComponent } from './board-container/board-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'board/:id',
+    component: BoardContainerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sticknotes-frontend/src/app/app.module.ts
+++ sticknotes-frontend/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BoardContainerComponent } from './board-container/board-container.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardContainerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,  
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
