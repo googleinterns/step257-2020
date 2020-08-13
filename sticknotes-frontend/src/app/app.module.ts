47c80f7cb64ce77465cc6627398f64dc457a2d66
AndrzejPodo <andrzejpodobinski2@gmail.com>
2020-08-13 13:24:50 +0000

    finish accesible board design

--- sticknotes-frontend/src/app/app.module.ts
+++ sticknotes-frontend/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AccessibleBoardsComponent } from './accessible-boards/accessible-boards.component';
+import { ElevateOnHoverDirective } from './elevate-on-hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccessibleBoardsComponent
+    AccessibleBoardsComponent,
+    ElevateOnHoverDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatGridListModule
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
