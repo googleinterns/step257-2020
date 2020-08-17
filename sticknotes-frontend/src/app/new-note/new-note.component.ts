a6d94f47c5e6ff54fbd3db4b0451587c1463bf00
PaulKovalov <soapmactavish799@gmail.com>
2020-08-17 15:56:57 +0300

    Create new note functionality

--- sticknotes-frontend/src/app/new-note/new-note.component.ts
+++ sticknotes-frontend/src/app/new-note/new-note.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Vector2 } from '../utility/vector';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { noSpacesValidator } from '../utility/util';
+import { CreateNoteData } from '../interfaces';
+import { NotesApiService } from '../services/notes-api.service';
 
 @Component({
   selector: 'app-new-note',
@@ -11,20 +13,72 @@ import { noSpacesValidator } from '../utility/util';
 })
 export class NewNoteComponent implements OnInit {
   private position: Vector2;
+  private boardKey: string;
+
+  // to disable button when the data is being sent to the server
+  public sendingData = false;
 
   public newNoteForm = new FormGroup({
     content: new FormControl('', [
       Validators.required,
       noSpacesValidator,
       Validators.maxLength(128) // just random number
+    ]),
+    options: new FormControl('1', [
+      Validators.required
     ])
   })
-  constructor(@Inject(MAT_DIALOG_DATA) private data: Vector2) {
-    // receive position of the note passed from board-component when opening the dialog
-    this.position = data;
+  constructor(@Inject(MAT_DIALOG_DATA) private data: { position: Vector2, boardKey: string },
+    private notesApiService: NotesApiService,
+    private dialogRef: MatDialogRef<NewNoteComponent>) {
+    // receive position and board id from board-component when opening the dialog
+    this.position = data.position;
+    this.boardKey = data.boardKey;
   }
 
   ngOnInit(): void {
   }
 
+  // creates new note
+  public createNote() {
+    if (this.newNoteForm.valid) {
+      // disable button
+      this.sendingData = true;
+      // construct note payload
+      const noteContent = this.newNoteForm.controls.content.value;
+      const noteColor = this.getColorHexValue(this.newNoteForm.controls.options.value);
+      const noteData: CreateNoteData = {
+        x: this.position.x,
+        y: this.position.y,
+        content: noteContent,
+        color: noteColor,
+        boardKey: this.boardKey
+      }
+      // service returns a new note object
+      this.notesApiService.createNote(noteData).subscribe(note => {
+        // successfully created, close the dialog and pass the note back to the board
+        this.dialogRef.close(note);
+      }, err => {
+        // something went wrong
+        alert('Error occurred');
+        this.dialogRef.close();
+      });
+    }
+  }
+
+  public getColorHexValue(val: string): string {
+    switch (val) {
+      case '1': {
+        return '#ffff99';
+      }
+      case '2': {
+        return '#ccfff5';
+      }
+      case '3': {
+        return '#ffe6ff';
+      } default: {
+        return '#e6e6ff';
+      }
+    }
+  }
 }
---
+++ sticknotes-frontend/src/app/services/notes-api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CreateNoteData, Note } from '../interfaces';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesApiService {
+
+  constructor(private http: HttpClient) { }
+
+  public createNote(note: CreateNoteData): Observable<Note> {
+    // comment this for now because we don't have backend yet
+    // return this.http.post<Note>('api/notes/', note);
+    // use mocked response for now
+    const mockedNoteResponse: Note = {
+      key: 'key',
+      creationDate: new Date().toISOString(),
+      creator: 'googler@google.com',
+      x: note.x,
+      y: note.y,
+      content: note.content,
+      color: note.color,
+      boardKey: note.boardKey
+    }
+    return of(mockedNoteResponse);
+  }
+}
