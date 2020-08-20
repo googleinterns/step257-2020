4eb9c3463b55392ad839f2f6a799cf2da6be5742
PaulKovalov <soapmactavish799@gmail.com>
2020-08-20 17:28:29 +0300

    Remove unused code

--- sticknotes-frontend/src/app/services/board-api.service.ts
+++ sticknotes-frontend/src/app/services/board-api.service.ts
@@ -1,29 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { Board, User, Note } from '../interfaces';
+import { Observable } from 'rxjs';
+import { Board } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardApiService {
-  private mockedUser: User = {
-    key: 'userKey',
-    nickname: 'googler',
-    email: 'googler@google.com',
-    accessibleBoards: []
-  };
-  private mockedNewBoardResponse: Board = {
-    key: 'boardKey',
-    notes: [],
-    users: [],
-    creationDate: this.currentTimestamp,
-    title: 'boardTitle',
-    creator: this.mockedUser,
-    rows: 4,
-    cols: 6,
-    backgroundImg: null
-  }
 
   constructor(private http: HttpClient) { }
 
@@ -32,10 +15,10 @@ export class BoardApiService {
   }
 
   public createBoard(boardTitle: string): Observable<Board> {
-    return this.http.post<Board>('api/board/', {title: boardTitle});
+    return this.http.post<Board>('api/board/', { title: boardTitle });
   }
 
   public updateBoardTitle(boardId: string, boardTitle: string): Observable<Board> {
-    return this.http.patch<Board>(`api/boards/?id=${boardId}`, {title: boardTitle});
+    return this.http.patch<Board>(`api/boards/?id=${boardId}`, { title: boardTitle });
   }
 }
