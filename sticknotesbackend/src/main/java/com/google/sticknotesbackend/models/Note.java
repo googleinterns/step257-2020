7a0df3719405244da0cc5241897bc65a900232ac
AndrzejPodo <andrzejpodobinski2@gmail.com>
2020-08-20 09:01:01 +0000

    first sketch of models

--- sticknotesbackend/src/main/java/com/google/sticknotesbackend/models/Note.java
+++ sticknotesbackend/src/main/java/com/google/sticknotesbackend/models/Note.java
@@ -1,4 +1,4 @@
-package com.google.sticknotesbackend;
+package com.google.sticknotesbackend.models;
 
 import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
@@ -9,30 +9,31 @@ import com.googlecode.objectify.annotation.Load;
 public class Note {
   @Id Long id;
   String content;
-  String creationDate;
+  String image;
+  Long creationDate; //timestamp here
   int x;
   int y;
   String color;
-  String creator;
-  @Load Ref<Board> board;
+  @Load Ref<User> creator;
+  @Load Ref<Whiteboard> board;
 
-  public Note() {
-    this.creator = "googler@google.com";
+  public Note(User user) {
+    this.creator = Ref.create(user);
   }
 
-  public Note(String content, String color, int x, int y) {
-    this.creator = "googler@google.com";
+  public Note(User user, String content, String color, int x, int y) {
+    this.creator = Ref.create(user);
     this.content = content;
     this.color = color;
     this.x = x;
     this.y = y;
   }
 
-  public Board getBoard() {
+  public Whiteboard getBoard() {
     return board.get();
   }
 
-  public void setBoard(Board board) {
+  public void setBoard(Whiteboard board) {
     this.board = Ref.create(board);
   }
 }
--- sticknotesbackend/src/main/java/com/google/sticknotesbackend/models/Whiteboard.java
+++ sticknotesbackend/src/main/java/com/google/sticknotesbackend/models/Whiteboard.java
@@ -1,8 +1,31 @@
 package com.google.sticknotesbackend.models;
 
+import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Load;
 
-
+@Entity
 public class Whiteboard {
-  
+  @Id Long id;
+  Long creationDate; //it's a timestamp
+  @Load Ref<User> creator;
+  String title;
+  int rows;
+  int cols;
+  String backgroundImg;
+
+  public Whiteboard() {}
+
+  public Whiteboard(String title) {
+    this.title = title;
+  }
+
+  public User getCreator(){
+    return creator.get();
+  }
+
+  public void setCreator(User user){
+    this.creator = Ref.create(user);
+  }
 }
