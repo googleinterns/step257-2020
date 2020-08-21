b19ae02760f96bf8aca39f2e9af8a726c109808d
PaulKovalov <soapmactavish799@gmail.com>
2020-08-21 16:34:31 +0300

    Connect frontend to notes backend

--- sticknotes-frontend/src/app/services/notes-api.service.ts
+++ sticknotes-frontend/src/app/services/notes-api.service.ts
@@ -11,34 +11,15 @@ export class NotesApiService {
   constructor(private http: HttpClient) { }
 
   public createNote(note: CreateNoteApiData): Observable<Note> {
-    // comment this for now because we don't have backend yet
-    // return this.http.post<Note>('api/notes/', note);
-    // use mocked response for now
-    const mockedNoteResponse: Note = {
-      id: 'id',
-      creationDate: this.currentTimestamp,
-      creator: 'googler@google.com',
-      x: note.x,
-      y: note.y,
-      content: note.content,
-      color: note.color,
-      boardId: note.boardId
-    };
-    return of(mockedNoteResponse);
+    return this.http.post<Note>('api/notes/', note);
   }
 
   public updateNote(note: Note): Observable<Note> {
-    // comment this for now because we don't have backend yet
-    // return this.http.post<Note>('api/notes/', note);
-    return of (note);
+    return this.http.post<Note>('api/edit-note/', note);
   }
 
   public deleteNote(noteId: string): Observable<void> {
     // return this.http.delete<void>(`api/notes?key=${noteId}`);
     return of(undefined);
-  }
-
-  get currentTimestamp(): string {
-    return Math.round(new Date().getTime()).toString()
   }
 }
