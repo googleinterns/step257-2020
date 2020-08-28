51d9b142f85fb9a2eb6c48857546f44d320c4ec2
PaulKovalov <soapmactavish799@gmail.com>
2020-08-28 15:25:51 +0300

    board live update

--- sticknotes-frontend/src/app/board/board.component.ts
+++ sticknotes-frontend/src/app/board/board.component.ts
@@ -59,24 +59,37 @@ export class BoardComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe(params => {
       const boardId = params.get('id'); // get board id from route param
       // load board with the key
-      this.boardApiService.getBoard(boardId).subscribe(board => {
-        this.board = board;
-        this.updateBoardAbstractGrid();
-        // pass essential board's data to the sidenav
-        const sidenavData: BoardData = {
-          id: board.id,
-          title: board.title,
-          creationDate: board.creationDate,
-          backgroundImg: board.backgroundImg,
-          rows: board.rows,
-          cols: board.cols,
-          creator: board.creator
-        };
-        this.boardLoaded.emit(sidenavData);
-      });
+      this.fetchBoardData(boardId);
+      // fetch board data each 5 seconds from the server
+      setInterval(() => {
+        this.fetchBoardData(boardId);
+      }, 5000);
     });
   }
 
+  /**
+   * Fetches the board data from the server.
+   * Initializes local variable board, sends data to the sidenav,
+   * updates abstract grid
+   */
+  private fetchBoardData(boardId: string) {
+    // load board with the key
+    this.boardApiService.getBoard(boardId).subscribe(board => {
+      this.board = board;
+      this.updateBoardAbstractGrid();
+      // pass essential board's data to the sidenav
+      const sidenavData: BoardData = {
+        id: board.id,
+        title: board.title,
+        creationDate: board.creationDate,
+        backgroundImg: board.backgroundImg,
+        rows: board.rows,
+        cols: board.cols,
+        creator: board.creator
+      };
+      this.boardLoaded.emit(sidenavData);
+    });
+  }
   // updates the z-index of the note
   public onNoteDragStart(cdkDragStart: CdkDragStart): void {
     const elementRef = cdkDragStart.source.element.nativeElement;
