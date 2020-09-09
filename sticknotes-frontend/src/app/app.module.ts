634b24a287d878b67fa755b2b7291ee1b1c43f0e
AndrzejPodo <andrzejpodobinski2@gmail.com>
2020-09-09 09:51:54 +0000

    basic edit role functionality

--- sticknotes-frontend/src/app/app.module.ts
+++ sticknotes-frontend/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { BoardEditComponent } from './board-edit/board-edit.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSelectModule } from '@angular/material/select';
 import { MaterialFileInputModule } from 'ngx-material-file-input';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { MaterialFileInputModule } from 'ngx-material-file-input';
     NewNoteComponent,
     RangeLoopPipe,
     AddUserComponent,
-    BoardEditComponent
+    BoardEditComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
